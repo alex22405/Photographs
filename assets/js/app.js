34d29d405ee16ff75e0a621cc86fc24db4d9b31e
avigon <vigonalexandre@hotmail.com>
2023-08-17 01:14:13 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+const sr = ScrollReveal();
+
+sr.reveal('.head_title', {
+    origin: 'left',
+    distance: '50px',
+    duration: 2000,
+});
+sr.reveal('.head_name', {
+    origin: 'right',
+    distance: '50px',
+    duration: 2000,
+});
+sr.reveal('.head_date', {
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    delay: 500,
+});
+sr.reveal('.head_hr_top', {
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    delay: 500,
+});
+sr.reveal('.head_hr_bottom', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 2000,
+    delay: 500,
+});
+sr.reveal('.head_see', {
+    origin: 'left',
+    distance: '50px',
+    duration: 2000,
+});
+sr.reveal('article', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 4000,
+    delay: 1000,
+});
+
